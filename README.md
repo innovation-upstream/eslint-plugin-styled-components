10f19361318df4ea0f2db007057020282edd4946
Justus Onuoha <justus@rubix.io>
2022-01-05 20:05:54 +0100

    fix loc precision update readme

--- README.md
+++ README.md
@@ -41,6 +41,7 @@ Then configure the rules you want to use under the rules section.
 
 ## Supported Rules
 
-* Fill in provided rules here
+* no-media-query
+* no-expression
 
 
--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@ module.exports= {
   isStyledComponent: (node) => {
     return node.tag.name === 'styled' ||
       (node.tag.callee && node.tag.callee.name === 'styled') ||
-      (node.tag.object && node.tag.object.name === 'styled');
+      (node.tag.object && node.tag.object.name === 'styled') ||
+      (node.tag.callee && node.tag.callee.object && node.tag.callee.object.object && node.tag.callee.object.object.name === 'styled');
   }
 }
